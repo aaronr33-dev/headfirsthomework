9d95122a2043839d1bf7304e46089f3da7ecefd2
aaronr33-dev <aaronm.robinson97@gmail.com>
2020-03-13 12:22:18 -0500

    Head first java homework. Pages 31, 34 and 37

---
+++ src/main/java/page31/Square.java
@@ -0,0 +1,17 @@
+package page31;
+
+public class Square {
+    public void rotate(){}
+    public void playSound(){}
+}
+
+class Circle{
+    public void rotate(){}
+    public void playSound(){}
+}
+
+class Triangle{
+    public void rotate(){}
+    public void playSound(){}
+}
+
